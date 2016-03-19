86d23eee3524a26c35c7c3dfe413b78630637451
Ellen Chisa <ellen.chisa@gmail.com>
2016-03-19 17:11:52 -0400

    Adding https for kickstarter

--- jobs/kickstarter.js
+++ jobs/kickstarter.js
@@ -2,7 +2,7 @@ job('last_kickstarter', function(done) {
 	var cheerio = require('cheerio');
 	var request = require('request');
 
-	request.get('http://www.kickstarter.com/profile/ellenchisa', function(err,response) {
+	request.get('https://www.kickstarter.com/profile/ellenchisa', function(err,response) {
 		var $ = cheerio.load(response.body);
 
 		var page = $('.page');
@@ -14,7 +14,7 @@ job('last_kickstarter', function(done) {
 
 		projects.forEach(function(project) {
 			var title = $(project).find('.project_name').first().text();
-			var url = "http://www.kickstarter.com/" + $(project).attr('href');
+			var url = "https://www.kickstarter.com/" + $(project).attr('href');
 			var image = $(project).find('img').first().attr('src');
 			result.push({
 				title:title,image:image,url:url
