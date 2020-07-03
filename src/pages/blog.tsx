9b0aa8ab542cafa127ac4a62bfe7ba8d8f5c8096
Ellen Chisa <ellen.chisa@gmail.com>
2020-07-03 15:39:48 -0400

    blog tweaks

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -11,7 +11,7 @@ const IndexPage = ({
     .filter(edge => !!edge.node.frontmatter.date) // You can filter your posts based on some criteria
     .map(edge => <PostLink key={edge.node.id} post={edge.node} />)
 
-  return <div>{Posts}</div>
+  return <div>All Posts:<br></br> {Posts}</div>
 }
 
 export default IndexPage
--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -10,7 +10,7 @@ export default function Template({
     <div className="blog-post-container">
       <div className="blog-post">
         <h1>{frontmatter.title}</h1>
-        <h2>{frontmatter.date}</h2>
+        <p>{frontmatter.date}</p>
         <div
           className="blog-post-content"
           dangerouslySetInnerHTML={{ __html: html }}
